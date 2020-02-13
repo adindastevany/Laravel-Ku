51e1628c0d544e74f9ca54aadc5b056a912f58e0
Adinda Putri Stevany 18 <adindaputristevani@gmail.com>
2020-02-13 10:45:05 +0700

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+
+class PagesController extends Controller
+{
+
+    public function home()
+    {
+        return view('index');
+    }
+}
